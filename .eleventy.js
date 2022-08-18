8da0ba156a020bb794ec9b8a6ba1a63a797c4774
Teranian <ewan@funkymatic.net>
2022-08-18 11:41:39 +1000

    added a non-smart display byKey

--- .eleventy.js
+++ .eleventy.js
@@ -56,7 +56,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addCollection("tunesAscending", (collection) =>
-    collection.getFilteredByGlob("_tunes/*.md").sort((a, b) => {
+    collection.getFilteredByGlob("tunes/*.md").sort((a, b) => {
       if (a.data.title > b.data.title) return -1;
       else if (a.data.title < b.data.title) return 1;
       else return 0;
